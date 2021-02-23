49c4b281f4a7ab77e9517f170385e770edb0317e
SeonghyeonShin <shinsh1932@naver.com>
2021-02-23 12:40:37 +0900

    mv test

--- tmp/크레인_인형뽑기_게임.py
+++ tmp/크레인_인형뽑기_게임.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from collections import deque
 
-
+tmp = 0
 """
 # best
 def solution(board, moves):
